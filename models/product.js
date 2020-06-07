b39b7fe5d25296d23849c686b4dbeab831da66ce
alexseveneight <alexseveneight@gmail.com>
2020-06-07 20:56:59 +0300

    'product_model'

--- models/product.js
+++ models/product.js
@@ -1,4 +1,8 @@
-const products = []
+const fs = require('fs');
+const path = require('path')
+
+const products = [];
+const way = path.join(__dirname, '../data', 'products.json')
 
 module.exports = class Product {
     constructor(title, imageUrl, price, description) {
@@ -8,7 +12,14 @@ module.exports = class Product {
         this.description = description;
     }
     save(){
-        products.push(this)
+        fs.readFile(way, (err, fileContent) => {
+            if(err) {
+                return [];
+            }
+            JSON.parse(fileContent);
+        });
+        products.push(this);
+        fs.writeFile(way, JSON.stringify(products))
     }
     static fetchAll(){
         return products;
