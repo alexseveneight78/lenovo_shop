60ccc2e9329e438279ee0cb460464c01a7dc0554
alexseveneight <alexseveneight@gmail.com>
2020-06-07 20:38:37 +0300

    'product_model'

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const path = require('path')
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+const shopRoute = require('./routes/shop');
+const adminRoute = require('./routes/admin')
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(shopRoute.route);
+app.use('/admin', adminRoute.route)
+
+app.listen(3000)
