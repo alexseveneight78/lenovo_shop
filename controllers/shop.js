0c03799ada80546c1f879287c085183f9d4ec458
alexseveneight <alexseveneight@gmail.com>
2020-06-09 14:54:12 +0300

    'shop_structure'

--- controllers/shop.js
+++ controllers/shop.js
@@ -18,6 +18,12 @@ exports.getCart = (req, res, next) => {
     )
 }
 
+exports.getOrders = (req, res, next) => {
+    res.render('shop/orders', { 
+        path: '/orders'}
+    )
+}
+
 exports.getCheckout = (req, res, next) => {
     res.render('shop/checkout', {
         path: '/checkout'
